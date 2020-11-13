809028c5310fe9038f3e7d66d3e097eba33b277e
stephen-goddard <williamgoddardmsg@gmail.com>
2020-11-13 14:27:46 +0000

    Added Item class

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,76 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+
+	private Long id;
+	private String name;
+	private Double cost;
+
+	public Item(String name, Double cost) {
+		this.name= name;
+		this.cost = cost;
+	}
+
+	public Item(Long id, String name, Double cost) {
+		this.id = id;
+		this.name= name;
+		this.cost = cost;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getCost() {
+		return cost;
+	}
+
+	public void setCost(Double cost) {
+		this.cost = cost;
+	}
+
+	@Override
+	public String toString() {
+		return "id: " + id + ", first name: " + name + ", cost: " + cost;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (cost == null) {
+			if (other.cost != null)
+				return false;
+		} else if (!cost.equals(other.cost))
+			return false;
+		return true;
+	}
+
+}
