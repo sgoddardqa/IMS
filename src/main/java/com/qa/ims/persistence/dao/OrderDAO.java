62c0f09bd28011feaf2cc8e02dd37533e95d03b1
stephen-goddard <williamgoddardmsg@gmail.com>
2020-11-16 15:42:53 +0000

    Added OrderDAO class

---
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -0,0 +1,131 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DBUtils;
+
+public class OrderDAO implements Dao<Order> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("order_id");
+		Long customer = resultSet.getLong("customer_id");
+		List<Long> items = new ArrayList<>();
+		return new Order(id, customer, items);
+	}
+
+	/**
+	 * Reads all orders from the database
+	 * 
+	 * @return A list of orders
+	 */
+	@Override
+	public List<Order> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");) {
+			List<Order> orders = new ArrayList<>();
+			while (resultSet.next()) {
+				orders.add(modelFromResultSet(resultSet));
+			}
+			return orders;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public Order readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders ORDER BY order_id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Creates an order in the database
+	 * 
+	 * @param order - takes in an order object. id will be ignored
+	 */
+	@Override
+	public Order create(Order order) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("INSERT INTO orders(customer_id) values('" + order.getCustomer() + "')");
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	public Order readItem(Long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders WHERE order_id = " + id);) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Updates an order in the database
+	 * 
+	 * @param order - takes in an order object, the id field will be used to
+	 *                 update that order in the database
+	 * @return
+	 */
+	@Override
+	public Order update(Order order) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("UPDATE orders SET customer_id = '" + order.getCustomer() + "' WHERE order_id =" + order.getId());
+			return readItem(order.getId());
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Deletes an order in the database
+	 * 
+	 * @param id - id of the order
+	 */
+	@Override
+	public int delete(long id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			return statement.executeUpdate("DELETE FROM orders WHERE order_id = " + id);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}
+
+}
